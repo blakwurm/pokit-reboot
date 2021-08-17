a1b42b747d8c8a2c1d57ccf81fce8fd36ea359d4
Jordan LaPrise <officialtoxicdub@gmail.com>
2021-08-16 16:55:31 -0800

    update test module to include some new features

--- source/modules/Test/main.ts
+++ source/modules/Test/main.ts
@@ -1,9 +1,21 @@
 import { handler, module } from "../../modloader.js";
+import { PokitOS } from "../../pokit.js";
 
 @module("Test")
 class WeirdClassName {
-  @handler("update")
+  engine: PokitOS;
+
+  constructor(engine: PokitOS) {
+    this.engine = engine;
+  }
+
+  @handler("postLoad")
   async neatocustomfuncname() {
     console.log(this);
+  }
+  
+  @handler()
+  async postUpdate() {
+    console.log("spin!");
   }
 }
