a850da5572d8fbc9f3e0c9521b49aaa322da9c9f
Jordan LaPrise <officialtoxicdub@gmail.com>
2021-08-18 13:55:40 -0800

    create test cart

---
+++ source/testcart/scripts/index.ts
@@ -0,0 +1,16 @@
+import { system } from "../../ecs.js";
+import { Entity } from "../../entity.js";
+
+window.Pokit.ecs.registerComponent("moveable", {
+  speed: 25
+});
+
+@system()
+class Move {
+  public defaultComponent = "moveable";
+  public priority = 0;
+
+  async update(entity: Entity) {
+
+  }
+}
