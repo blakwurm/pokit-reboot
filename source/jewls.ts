ac763d9cd8cb26700a05864bbcfdb473f803c2b9
Jordan LaPrise <officialtoxicdub@gmail.com>
2021-08-18 18:15:21 -0800

    remove jewls renderer stand in

--- source/jewls.ts
+++
@@ -1,7 +0,0 @@
-import { CullingFunction, Renderer } from "./pokit"
-
-export class Jewls implements Renderer {
-  render(cullFunc?: CullingFunction): void {
-    throw new Error("Method not implemented.");
-  }
-}
