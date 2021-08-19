253b4d483a5a2a98c7f9d408336f010d657a549d
Zaphodious <achythlook@blakwurm.com>
2021-08-19 18:38:51 +0000

    Update location of input api

---
+++ source/modules/Engine/input/input.ts
@@ -0,0 +1,25 @@
+import { api } from "../../../modloader";
+
+@api()
+class InputMod extends Map<string, Number> {
+	constructor() {
+		super()
+		for (let n of [
+			'up',
+			'down',
+			'left',
+			'right',
+			'x',
+			'y',
+			'b',
+			'a',
+			'l',
+			'r',
+			'start',
+			'select',
+			'opt'
+		]) {
+			this.set(n, 0)
+		}
+	}
+}
