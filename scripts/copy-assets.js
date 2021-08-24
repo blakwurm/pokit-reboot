be11cf9d6bcf3aa5d21fb96a4a22daca6fd4d345
Jordan LaPrise <jlaprise@blakwurm.com>
2021-08-23 20:17:35 -0800

    make init scripts in preperation for templates

--- scripts/copy-assets.js
+++ scripts/copy-assets.js
@@ -4,20 +4,31 @@ let fs = require('fs');
 let path = require('path');
 let Glob = require('glob');
 
-let arr = process.argv;
-let dest = arr.pop();
-let glob = arr.pop();
-let source = arr.pop();
+async function copy(source, glob, dest) {
+  return new Promise((resolve) => {
+    new Glob(path.join(source,glob), (_,files)=>{
+      for(let f of files) {
+        if(fs.lstatSync(f).isDirectory())continue;
+        let dir = path.dirname(f).substr(source.length);
+        dir = path.join(dest, dir);
+        fs.mkdirSync(dir, {recursive:true});
+        dir = path.join(dir, path.basename(f));
+        let o = path.join(f);
+        console.log(o, "=>", dir);
+        fs.copyFileSync(o, dir);
+      }
+      resolve();
+    });
+  });
+}
 
-new Glob(path.join(source,glob), (_,files)=>{
-  for(let f of files) {
-    if(fs.lstatSync(f).isDirectory())continue;
-    let dir = path.dirname(f).substr(source.length);
-    dir = path.join(dest, dir);
-    fs.mkdirSync(dir, {recursive:true});
-    dir = path.join(dir, path.basename(f));
-    let o = path.join(f);
-    console.log(o, "=>", dir);
-    fs.copyFileSync(o, dir);
-  }
-});
+module.exports = copy;
+
+if(process.argv.length > 3) {
+  let arr = process.argv;
+  let dest = arr.pop();
+  let glob = arr.pop();
+  let source = arr.pop();
+
+  copy(source, glob, dest);
+}
---
+++ scripts/init-cart.js
@@ -0,0 +1,6 @@
+let copy = require('./copy-assets');
+
+let glob = "**/*";
+if(process.argv.length > 1 && process.argv[1] === "nogitignore") glob += "[!.gitignore]";
+
+copy("../templates/cart", glob, process.cwd());
