078dc425580f8b7f5dd52deeb1dbffdd8f445dd0
Jordan LaPrise <officialtoxicdub@gmail.com>
2021-09-17 09:07:27 -0800

    setup testcart for physcis

--- source/pokit/testcart/scripts/index.ts
+++ source/pokit/testcart/scripts/index.ts
@@ -1,6 +1,6 @@
 import { system } from "../../ecs.js";
 import { Entity } from "../../entity.js";
-import { Identity } from "../../pokit.js";
+import { Identity, PokitOS } from "../../pokit.js";
 
 window.Pokit.ecs.registerComponent("moveable", {
   speed: 25
@@ -11,13 +11,32 @@ class Move {
   public defaultComponent = "moveable";
   public priority = 0;
 
+  engine: PokitOS;
+  input?: Map<string,number>;
+
+  constructor(engine: PokitOS) {
+    this.engine = engine;
+  }
+
   async init(entity: Entity) {
     console.log("Init!");
+    this.input = this.engine.modules.get("input");
   }
 
   async update(entity: Entity) {
-    //console.log("Wee!");
-    entity.rotation +=5;
+    let move = entity.get("moveable");
+    if(this.input!.get("up")) entity.position.y -= move.speed;
+    if(this.input!.get("down")) entity.position.y += move.speed;
+    if(this.input!.get("left")) entity.position.x -= move.speed;
+    if(this.input!.get("right")) entity.position.x += move.speed;
+  }
+
+  async onCollisionEnter() {
+    console.log("enter");
+  }
+
+  async onCollisionExit() {
+    console.log("exit");
   }
 
   async destroy(entity: Entity) {
