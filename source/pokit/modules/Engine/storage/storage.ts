f0ace92aae39d426bd9e8489ad1194cf54d674a1
Zaphodious <achythlook@blakwurm.com>
2021-10-02 23:25:41 -0800

    make settings list return value

--- source/pokit/modules/Engine/storage/storage.ts
+++ source/pokit/modules/Engine/storage/storage.ts
@@ -22,6 +22,7 @@ export class Settings {
             list: ()=>{
                 let arr: string[] = []
                 this.engine.modules.callEvent('querySettings', ns, arr);
+                return arr;
             }
         }
     }
