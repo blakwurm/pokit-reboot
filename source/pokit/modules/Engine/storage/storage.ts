3fe165f8bc8ebb8b48d04a642c1fba2df08d585d
Jordan LaPrise <jlaprise@blakwurm.com>
2021-10-02 22:16:13 -0800

    add settings framework

---
+++ source/pokit/modules/Engine/storage/storage.ts
@@ -0,0 +1,24 @@
+import { IJsonTypes, PokitOS } from "../../../pokit"
+
+@api()
+class Settings {
+    engine: PokitOS
+
+    constructor(engine:PokitOS) {
+        this.engine = engine;
+    }
+
+    path(namespace: string) {
+        let ns = namespace.endsWith('/') ? namespace : namespace + '/'
+        return {
+            get: (name: string) => {
+                let obj = {value: {} as IJsonTypes};
+                this.engine.modules.callEvent('loadSetting', ns + name, obj);
+                return obj.value;
+            },
+            set: (name: string, value: IJsonTypes) => {
+                this.engine.modules.callEvent('saveSetting', ns + name, value);
+            }
+        }
+    }
+}
